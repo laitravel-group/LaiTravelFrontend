72a516b2ed521ba5bcc28fecdb606f827b544378
Tonyxks <xue980902@gmail.com>
2023-07-03 20:07:33 -0600

    Update User.js
    updated the list of the plan history

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,62 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
 import { Link } from "react-router-dom";
 import { Button, Col } from "antd";
-import PageHeader from "../components/PageHeader";
-import User from "./pages/User";
-import MyPlans from "./pages/MyPlans";
-import NewPlan from "./pages/NewPlan";
+import { LogoutOutlined } from "@ant-design/icons";
 
 const { Content } = Layout;
 
-export default function User(props) {
+export default function UserPage(props) {
+  const [showProfilePanel, setShowProfilePanel] = useState(true);
+  const [showMyPlansPanel, setShowMyPlansPanel] = useState(false);
+
+  const handleMyPlansClick = () => {
+    setShowProfilePanel(false);
+    setShowMyPlansPanel(true);
+  };
+
+  const handleProfileClick = () => {
+    setShowProfilePanel(true);
+    setShowMyPlansPanel(false);
+  };
+
   return (
     <Layout style={{ height: "100vh" }}>
       <Col className="home-panel">
         <Content style={{ marginTop: "50px" }}>
           <Link to="/new_plan">
-            <Button type="primary" id="home-new-trip-button">
+            <Button
+              type="primary"
+              id="home-new-trip-button"
+              className="yellow-button"
+            >
               New Plan
             </Button>
           </Link>
 
           <Link to="/my_plans">
-            <Button type="primary" id="user-my-trip-plans-history-button">
+            <Button
+              type="primary"
+              id="user-my-trip-button"
+              className="yellow-button"
+              onClick={handleMyPlansClick}
+            >
               My Plan
             </Button>
           </Link>
 
           <Link to="/profile">
-            <Button type="primary" id="user-profile-button">
+            <Button
+              type="primary"
+              id="user-profile-button"
+              className="yellow-button"
+              onClick={handleProfileClick}
+            >
               Profile
             </Button>
           </Link>
 
           <Link to="/edit_profile">
-            <Button type="primary" id="user-edit-profile-button">
+            <Button
+              type="primary"
+              id="user-edit-profile-button"
+              className="yellow-button"
+              onClick={handleProfileClick}
+            >
               Edit Profile
             </Button>
           </Link>
         </Content>
       </Col>
 
-      <div className="profile-panel">
-        <div className="profile-info">
-          <div className="profile-picture"></div>
-          <div className="user-name">XXX XX</div>
-          <div className="email">XXXXX@example.com</div>
+      {showProfilePanel && (
+        <div className="profile-panel">
+          <div className="profile-info">
+            <div className="profile-picture"></div>
+            <div className="user-name">XXX XX</div>
+            <div className="email">XXXXX@example.com</div>
+          </div>
+          <Button
+            type="primary"
+            className="sign-out-button"
+            icon={<LogoutOutlined />}
+          >
+            Sign Out
+          </Button>
         </div>
-        <Button
-          type="primary"
-          className="sign-out-button"
-          icon={<LogoutOutlined />}
-        >
-          Sign Out
-        </Button>
-      </div>
+      )}
+
+      {showMyPlansPanel && (
+        <div className="yellow-panel">
+          <div className="yellow-panel-content">
+            List of trip plans historys
+          </div>
+        </div>
+      )}
 
       <style>
         {`
+          .home-panel button {
+            background-color: yellow;
+            color: black;
+          }
+
+          .yellow-panel {
+            position: fixed;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%);
+            width: 200px;
+            background-color: yellow;
+            padding: 20px;
+          }
+
+          .yellow-panel-content {
+            text-align: center;
+          }
+
           .profile-panel {
             position: fixed;
             right: 0;
