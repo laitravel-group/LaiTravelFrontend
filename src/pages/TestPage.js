09b7e5dabe2c654f83a63b6ab9c015a1974a366a
wiic123 <136217526+wiic123@users.noreply.github.com>
2023-07-31 01:01:06 -0700

    Implemented TripPlanDetailsPerDay editor
    Implemented PlaceVisitEdit component to display and modify TripPlanDetailsPerDay.
    PlaceVisitEdit takes 2 parameters. First parameter is tripPlan, which is the full TripPlan object. Second one is day, which indicate which TripPlanDetailsPerDay to display; It's the index of details array, starting from 0.
    Pending map marker function.

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import PlaceVisitEdit from "../components/new-plan/PlaceVisitEdit";
 import { tripPlanJson } from "../models/testData";
 import { TripPlan } from "../models/tripPlan";
 import OptimizePlan from "../components/OptimizePlan";
 
 export default function TestPage() {
 	const tripPlan = TripPlan.fromJson(tripPlanJson);
-	return <OptimizePlan tripPlan={tripPlan} />;
+	console.log(tripPlan);
+	return (
+		<>
+			<PlaceVisitEdit tripPlan={tripPlan} day={1} />
+		</>
+	);
 }
