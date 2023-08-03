4e810d0568b76c73334ff7a99949f7581c6a3cc7
stop-x13 <stop.x13@gmail.com>
2023-08-03 21:44:09 +1000

    Remove duplicate line

--- src/components/new-plan/NewPlanBuild.js
+++ src/components/new-plan/NewPlanBuild.js
@@ -12,7 +12,6 @@ import { ExclamationCircleFilled, CalendarOutlined } from "@ant-design/icons";
 import GoogleMap from "google-maps-react-markers";
 import PageHeader from "../PageHeader";
 import apiKey from "../../key";
-import apiKey from "../../key";
 import SearchBox from "./SearchBox";
 import ScrollableBox from "../ScrollableBox";
 import TripPlanTabs from "./TripPlanTabs";
